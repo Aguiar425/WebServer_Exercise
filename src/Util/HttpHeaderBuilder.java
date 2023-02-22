b1727abbb8edc2c8959b457b90bd3bb9dce0b7bc
Nuno Gil Mesquita <nunomesquita2@gmail.com>
2023-02-22 16:28:18 +0000

    Working on sending files

---
+++ src/Util/HttpHeaderBuilder.java
@@ -0,0 +1,43 @@
+package Util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class HttpHeaderBuilder {
+
+    public static String notAllowed() {
+        return "HTTP/1.1 Method 405 Not allowed";
+    }
+
+    public static String notFound(String fileName/*, long length*/) {
+        return "HTTP/1.1 404 Not Found\r\n" +
+                getContentType(fileName) +
+                "Content-Length: " + /*length*/ "\r\n\r\n";
+    }
+
+    public static String ok(String fileName/*, long length*/) {
+        return "HTTP/1.1 200 Document Follows\r\n" +
+                getContentType(fileName) +
+                "Content-Length: " + /*length*/ "\r\n\r\n";
+    }
+
+    private static String getContentType(String fileName) {
+        String contentType = "";
+        try {
+            contentType = Files.probeContentType(Path.of("webresources" + fileName));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return contentType;
+    }
+
+    public static String badRequest() {
+        return "HTTP/1.1 400 Bad request\r\n";
+    }
+
+    public static String notSupported() {
+        return "HTTP/1.1 505 HTTP Version not supported\r\n";
+    }
+}
--- src/WebServer.java
+++ src/WebServer.java
@@ -1,83 +1,56 @@
-import com.sun.net.httpserver.HttpServer;
+import Util.HttpHeaderBuilder;
 
 import java.io.*;
-import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WebServer {
     public static void main(String[] args) throws IOException {
-        //HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        new WebServer().start();
+        int port = 8080;
+
+        new WebServer().start(port);
     }
 
-    public void start() throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
-        Socket clientSocket = serverSocket.accept();
-        RequestHandler requestHandler = new RequestHandler(clientSocket);
+    public void start(int port) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
 
-        while (true){
-            requestHandler.dealWithRequest();
+        ExecutorService service = Executors.newCachedThreadPool();
+
+        while (true) {
+            serveRequests(serverSocket, service);
         }
     }
 
-    public static class RequestHandler implements Runnable{
+    public void serveRequests(ServerSocket socket, ExecutorService service) throws IOException {
+        Socket clientSocket = socket.accept();
+
+        RequestHandler requestHandler = new RequestHandler(clientSocket);
+
+        service.submit(requestHandler);
+    }
+
+    public static class RequestHandler implements Runnable {
         Socket clientSocket;
 
         BufferedReader input;
+
         BufferedWriter output;
 
         String methodVerb;
+
         String path;
+
         String protocol;
 
         public RequestHandler(Socket clientSocket) throws IOException {
             this.clientSocket = clientSocket;
             this.input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             this.output = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-        }
-
-        public void dealWithRequest() throws IOException {
-            String[] inputArray = input.readLine().split(" ");
-
-            if(inputArray.length != 3){
-                System.out.println("Invalid Request");
-                return;
-            }
-            this.methodVerb = inputArray[0].toLowerCase();
-            this.path = "src" + inputArray[1].toLowerCase();
-            this.protocol = inputArray[2].toLowerCase();
-
-            if(validateRequests()){
-                System.out.println("OK");
-                //HttpHeaderBuilder.ok()
-            }
-        }
-
-        private Boolean validateRequests() {
-            if(!methodVerb.equals("get")){
-                System.out.println("Invalid verb \nOnly Allows: GET");
-                return false;
-            }
-            if (!path.startsWith("src/")){
-                System.out.println("Invalid Path Request");
-                return false;
-            }else {
-                Path directory = Path.of(path);
-                if(Files.notExists(directory)){
-                    System.out.println("File doesn't exist");
-                    return false;
-                    //todo send 404
-                }
-            }
-            if (!protocol.equals("http/1.1")){
-                System.out.println("InvalidProtocol");
-                return false;
-            }
-            return true;
         }
 
         @Override
@@ -88,5 +61,76 @@ public class WebServer {
                 throw new RuntimeException(e);
             }
         }
+
+        public void dealWithRequest() throws IOException {
+            String[] inputArray = input.readLine().split(" ");
+
+            if (inputArray.length != 3) {
+                System.out.println("Invalid Request");
+                send(HttpHeaderBuilder.notAllowed());
+                return;
+            }
+
+            this.methodVerb = inputArray[0].toLowerCase();
+
+            this.path = inputArray[1].toLowerCase();
+
+            this.protocol = inputArray[2].toLowerCase();
+
+            if (validateRequests()) {
+                sendFile();
+            }
+        }
+
+        private void sendFile() throws IOException {
+            send(HttpHeaderBuilder.ok(path));
+            Path.of(path);
+            File file = new File(path);
+            Scanner scanner = new Scanner(file);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                stringBuilder.append(scanner.nextLine());
+                stringBuilder.append("\n");
+            }
+            scanner.close();
+            send(stringBuilder.toString());
+        }
+
+        private Boolean validateRequests() throws IOException {
+            if (!methodVerb.equals("get")) {
+                send(HttpHeaderBuilder.notAllowed());
+                return false;
+            }
+            if (!path.startsWith("/")) {
+                send(HttpHeaderBuilder.badRequest());
+                System.out.println("Invalid Path Request");
+                return false;
+            } else {
+                Path directory = Path.of("src/webresources" + path);
+                if (Files.notExists(directory)) {
+                    send(HttpHeaderBuilder.notFound(path));
+                    System.out.println("File doesn't exist");
+                    return false;
+                }
+            }
+            if (!protocol.equals("http/1.1")) {
+                send(HttpHeaderBuilder.notSupported());
+                System.out.println("InvalidProtocol");
+                return false;
+            }
+            return true;
+        }
+
+        public void send(String message) {
+            try {
+                output.write(message);
+                output.newLine();
+                output.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
